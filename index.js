0b2396d6e678cf629796bdc2c51debd716b1b61e
Mark Percy <mark.percy98@outlook.com>
2023-04-20 15:17:15 +0100

    Added a get sites call to group sites by area

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.get('/', (req, resp) => {
         Welcome: 'Welcome to my api for more advanced requests to the met api'
     });
 });
+
 app.get('/v1/sites/region', (req, resp) => {
     const sites = new Sites();
     sites.getSitesForRegion(req.query).then((data) => {
@@ -27,11 +28,19 @@ app.get('/v1/sites/region', (req, resp) => {
 app.get('/check', (req, resp) => {
     resp.send({system:true});
 })
+
 app.get('/v1/sites/auth-area', (req, resp) => {
     const sites = new Sites();
     sites.getSitesForAuthArea(req.query).then((data) => {
         resp.send(data);
     });
+});
+
+app.get('/v1/sites/sites', (req, resp) => {
+    let sites = new Sites();
+    sites.getSites().then(data => {
+        resp.send(data)
+    })
 });
 
 app.listen(
--- src/v1/sites/sites.js
+++ src/v1/sites/sites.js
@@ -7,17 +7,39 @@ export class Sites {
         this.url.searchParams.append('key', environment.keys.weatherKey);
     }
 
+    async getSitesData() {
+        return (await fetch(this.url)).json();        
+    }
+
     async getSitesForRegion(query) {
-        let resp = await fetch(this.url);
-        let jsonData = await resp.json();
+        let jsonData = (await this.getSitesData());
         let filteredData = jsonData["Locations"]["Location"].filter(el => el["region"] == query.loc);
         return {data: filteredData};
     }
  
     async getSitesForAuthArea(query) {
-        let resp = await fetch(this.url);
-        let jsonData = await resp.json();
-        let filteredData = jsonData["Locations"]["Location"].filter(el => el["unitaryAuthArea"] == query.loc);
+        let jsonData = (await this.getSitesData());
+        let filteredData = jsonData.Locations.Location.filter(el => el["unitaryAuthArea"] == query.loc);
         return {data: filteredData};
+    }
+
+    async getSites() {
+        let jsonData = (await this.getSitesData()).Locations.Location;
+        let filteredData = jsonData.filter(site => site.unitaryAuthArea != null);
+        let LocationData = new Map()
+        filteredData.forEach(site => {
+            if(!LocationData.has(site.region)) LocationData.set(site.region, new Map())
+            if(!LocationData.get(site.region).has(site.unitaryAuthArea)) LocationData.get(site.region).set(site.unitaryAuthArea, [])
+            LocationData.get(site.region).get(site.unitaryAuthArea).push({name:site.name, id: site.id})
+        })
+        const json = JSON.stringify([...LocationData].reduce((obj, [key, value]) => {
+            obj[key] = [...value.entries()].reduce((subObj, [subKey, subValue]) => {
+              subObj[subKey] = subValue;
+              return subObj;
+            }, {});
+            return obj;
+          }, {}));
+
+        return json
     }
 }
