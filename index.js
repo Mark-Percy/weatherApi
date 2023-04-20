eaa84955391d536b93670cade745fdb80e5410f9
Mark Percy <mark.percy98@outlook.com>
2023-04-20 12:47:18 +0100

    Added cors to allow requests from same url for when docker angular app uses it

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 import { Sites } from "./src/v1/sites/sites.js"
 import express from "express"
-// const app = require('express')();
+import cors from 'cors'
 
-const app = express() 
+const app = express();
+
+const corsOptions = {
+    origin: 'http://localhost:4200'
+};
+  
+app.use(cors(corsOptions));
 
 const PORT = 3030;
 
 app.get('/', (req, resp) => {
     resp.send({
         Welcome: 'Welcome to my api for more advanced requests to the met api'
-    })
+    });
 });
 app.get('/v1/sites/region', (req, resp) => {
-    const sites = new Sites()
+    const sites = new Sites();
     sites.getSitesForRegion(req.query).then((data) => {
-        resp.send(data)
-    })
+        resp.send(data);
+    });
 });
+
+app.get('/check', (req, resp) => {
+    resp.send({system:true});
+})
 app.get('/v1/sites/auth-area', (req, resp) => {
-    const sites = new Sites()
+    const sites = new Sites();
     sites.getSitesForAuthArea(req.query).then((data) => {
-        resp.send(data)
-    })
+        resp.send(data);
+    });
 });
 
 app.listen(
     PORT,
     () => console.log(`Server Started on http://localhost:${PORT}`)
-)
+);
--- src/v1/sites/sites.js
+++ src/v1/sites/sites.js
@@ -1,23 +1,23 @@
-import { key } from "../environment/MApiKey.js"
+import { environment } from "../environments/environment.js"
 
 export class Sites {
-    url = new URL('http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist')
+    url = new URL(environment.weatherApiUrl);
 
     constructor() {
-        this.url.searchParams.append('key', key)
+        this.url.searchParams.append('key', environment.keys.weatherKey);
     }
 
     async getSitesForRegion(query) {
-        let resp = await fetch(this.url)
-        let jsonData = await resp.json()
-        let filteredData = jsonData["Locations"]["Location"].filter(el => el["region"] == query.loc)
-        return {data: filteredData}
+        let resp = await fetch(this.url);
+        let jsonData = await resp.json();
+        let filteredData = jsonData["Locations"]["Location"].filter(el => el["region"] == query.loc);
+        return {data: filteredData};
     }
  
     async getSitesForAuthArea(query) {
-        let resp = await fetch(this.url)
-        let jsonData = await resp.json()
-        let filteredData = jsonData["Locations"]["Location"].filter(el => el["unitaryAuthArea"] == query.loc)
-        return {data: filteredData}
+        let resp = await fetch(this.url);
+        let jsonData = await resp.json();
+        let filteredData = jsonData["Locations"]["Location"].filter(el => el["unitaryAuthArea"] == query.loc);
+        return {data: filteredData};
     }
 }
